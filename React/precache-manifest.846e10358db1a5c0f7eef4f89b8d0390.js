45bf002da0c20e786223b9465ffcebd262d9300e
ecorpnu <ecorpnu@gmail.com>
2022-02-12 18:05:38 +0800

    Add files via upload

---
+++ React/precache-manifest.846e10358db1a5c0f7eef4f89b8d0390.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e16c8640b82f2ef51e68c427aa3392c6",
+    "url": "/static/media/GetMetamask.e16c8640.png"
+  },
+  {
+    "revision": "4bb55d00f0135c7a2e57",
+    "url": "/static/css/main.4719ad83.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "5a700131481a9612e5d0",
+    "url": "/static/js/2.056c8934.chunk.js"
+  },
+  {
+    "revision": "b09a481842129c7a3f69ff5e76a6e1a7",
+    "url": "/static/media/skull-crossbones.b09a4818.svg"
+  },
+  {
+    "revision": "aef6f7d265d9f21b735320aa929e0d7a",
+    "url": "/static/media/check-circle.aef6f7d2.svg"
+  },
+  {
+    "revision": "4bb55d00f0135c7a2e57",
+    "url": "/static/js/main.2c77f01b.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "5a700131481a9612e5d0",
+    "url": "/static/css/2.e8071a47.chunk.css"
+  },
+  {
+    "revision": "80c02489b76102dacd70406c986f1e08",
+    "url": "/index.html"
+  }
+];
